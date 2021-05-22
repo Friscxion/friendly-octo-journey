7830f32af6450f3df2d451f4dbaa10d6a5fc0ef2
Friscxion <erwann.champion@outlook.fr>
2021-05-22 22:21:19 +0200

    Update

--- router.js
+++ router.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 let _LOCK_ = false;
+let status = "open";
 
 router.get('/open', (req, res) => {
     const {exec} = require('child_process');
-    if(_LOCK_) return res.send("nop");
+    if(_LOCK_) return res.send("denied");
     _LOCK_ = true
-
+    status="open";
     exec('python3 ./open.py', (err, stdout, stderr) => {
             if (err) console.error(`exec error: ${err}`);
             _LOCK_ = false;
@@ -18,9 +19,9 @@ router.get('/open', (req, res) => {
 
 router.get('/close', (req, res) => {
     const {exec} = require('child_process');
-    if(_LOCK_) return res.send("nop");
+    if(_LOCK_) return res.send("denied");
     _LOCK_ = true
-
+    status="closed"
     exec('python3 ./close.py', (err, stdout, stderr) => {
             if (err) console.error(`exec error: ${err}`);
             _LOCK_ = false;
@@ -30,8 +31,12 @@ router.get('/close', (req, res) => {
 })
 
 router.get('/pending', (req, res) => {
-    if(_LOCK_) return res.send("nop");
+    if(_LOCK_) return res.send("denied");
     res.send("ok")
+})
+
+router.get('/status', (req, res) => {
+    res.send(status)
 })
 
 module.exports=router;
