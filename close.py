d70f15c9a295db3a1d0afda8b06912bb691c7741
Friscxion <erwann.champion@outlook.fr>
2021-05-22 23:09:00 +0100

    your message

--- close.py
+++ close.py
@@ -1,3 +1,22 @@
-import time
-time.sleep(10)
-print('Salut')
+from time import sleep
+from gpiozero import Button
+import RPi.GPIO as IO
+
+IO.setwarnings(False)
+
+IO.setmode(IO.BCM)
+
+IO.setup(18,IO.OUT)
+
+left = IO.PWM(18,100)
+
+button = Button(16)
+
+left.start(0)
+left.ChangeDutyCycle(100)
+
+while True:
+    if(button.is_pressed):
+        print("Pressed")
+        left.stop()
+        break;
--- open.py
+++ open.py
@@ -1,3 +1,23 @@
-import time
-time.sleep(10)
-print('Salut')
+from time import sleep
+from gpiozero import Button
+import RPi.GPIO as IO
+
+IO.setwarnings(False)
+
+IO.setmode(IO.BCM)
+
+IO.setup(14,IO.OUT)
+
+right = IO.PWM(14,100)
+
+button = Button(21)
+
+right.start(0)
+right.ChangeDutyCycle(100)
+
+while True:
+    if button.is_pressed:
+        print("Pressed")
+        right.stop()
+        break;
+
