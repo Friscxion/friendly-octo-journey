1692f92afae30ec0ae5c9673379354104500e981
Friscxion <erwann.champion@outlook.fr>
2021-05-26 20:04:25 +0200

    Router

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const app = express()
 const port = 2021
 const router = require('./router')
 
+app.use(express.json())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(router); // your middleware function must be above the router
 
 app.listen(port, () => {
